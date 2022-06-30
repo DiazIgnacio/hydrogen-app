f209cd8bd703ef00ff490f98ba456e9d81e8764b
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-06-30 07:02:39 -0300

    fix: add Link to All Products Navbar Button

--- src/components/Button2/index.jsx
+++ src/components/Button2/index.jsx
@@ -1,26 +1,29 @@
-import React, { useEffect } from "react";
-import * as MuiMaterial from "@mui/material";
-import * as EmotionReact from "@emotion/react";
-import * as EmotionStyled from "@emotion/styled";
-import * as MuiIconsMaterial from "@mui/icons-material";
-import "./Button2.css";
+import React, { useEffect } from 'react';
+import * as MuiMaterial from '@mui/material';
+import { Link } from '@shopify/hydrogen';
+import * as EmotionReact from '@emotion/react';
+import * as EmotionStyled from '@emotion/styled';
+import * as MuiIconsMaterial from '@mui/icons-material';
+import './Button2.css';
 
 function Button2(props) {
   const { className } = props;
 
   return (
-    <div className={`products-button ${className || ""}`}>
-      <MuiMaterial.Button
-        endIcon=""
-        startIcon=""
-        size="medium"
-        color="primary"
-        disabled={false}
-        variant="contained"
-        disableElevation={false}
-      >
-        All Products
-      </MuiMaterial.Button>
+    <div className={`products-button ${className || ''}`}>
+      <Link to='/products-page-desktop'>
+        <MuiMaterial.Button
+          endIcon=''
+          startIcon=''
+          size='medium'
+          color='primary'
+          disabled={false}
+          variant='contained'
+          disableElevation={false}
+        >
+          All Products
+        </MuiMaterial.Button>
+      </Link>
     </div>
   );
 }
--- src/components/Button3/index.jsx
+++ src/components/Button3/index.jsx
@@ -1,26 +1,29 @@
-import React, { useEffect } from "react";
-import * as MuiMaterial from "@mui/material";
-import * as EmotionReact from "@emotion/react";
-import * as EmotionStyled from "@emotion/styled";
-import * as MuiIconsMaterial from "@mui/icons-material";
-import "./Button3.css";
+import React, { useEffect } from 'react';
+import * as MuiMaterial from '@mui/material';
+import { Link } from '@shopify/hydrogen';
+import * as EmotionReact from '@emotion/react';
+import * as EmotionStyled from '@emotion/styled';
+import * as MuiIconsMaterial from '@mui/icons-material';
+import './Button3.css';
 
 function Button3(props) {
   const { className } = props;
 
   return (
-    <div className={`cart-button ${className || ""}`}>
-      <MuiMaterial.Button
-        endIcon=""
-        size="medium"
-        color="primary"
-        disabled={false}
-        variant="outlined"
-        disableElevation={false}
-        startIcon={<MuiIconsMaterial.ShoppingCart />}
-      >
-        My Cart
-      </MuiMaterial.Button>
+    <div className={`cart-button ${className || ''}`}>
+      <Link to='/cart-page-desktop'>
+        <MuiMaterial.Button
+          endIcon=''
+          size='medium'
+          color='primary'
+          disabled={false}
+          variant='outlined'
+          disableElevation={false}
+          startIcon={<MuiIconsMaterial.ShoppingCart />}
+        >
+          My Cart
+        </MuiMaterial.Button>
+      </Link>
     </div>
   );
 }
