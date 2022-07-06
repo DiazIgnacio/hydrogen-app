93689f9f6246aa26ac73789be0b9f3751d6d2a57
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-07-06 03:43:33 -0300

    fix: config file

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,9 +1,9 @@
-import {defineConfig} from '@shopify/hydrogen/config';
+import { defineConfig } from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'anima-example.myshopify.com/',
-    storefrontToken: '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',
+    storeDomain: 'anima-test-example.myshopify.com/',
+    storefrontToken: 'caac6b2e876521f4306bd0d4826d3301',
     storefrontApiVersion: '2022-07',
   },
 });
