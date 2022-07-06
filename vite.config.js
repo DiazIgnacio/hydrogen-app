3c92f70c0e94446da7e3b3fb0f812f86561dd769
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-07-05 21:10:41 -0300

    refactor: default config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
-// import react from '@vitejs/plugin-react';
 import netlifyPlugin from '@netlify/hydrogen-platform/plugin';
 
 export default defineConfig({
-  plugins: [
-    // react(),
-    hydrogen({
-      babel: {
-        plugins: [
-          '@babel/plugin-transform-react-jsx',
-          { runtime: 'automatic' },
-        ],
-      },
-    }),
-    netlifyPlugin(),
-  ],
+  plugins: [hydrogen(), netlifyPlugin()],
 });
