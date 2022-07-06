00c3eadd230078ea92b2484c7ac163eb44d3bce7
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-07-06 03:18:11 -0300

    add shopify custom store token and connect with shopify api

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,8 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'anima-example.myshopify.com/',
+    storefrontToken: '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',
     storefrontApiVersion: '2022-07',
   },
 });
--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -2,6 +2,22 @@ import React from 'react';
 import HomePageDesktop from '../components/HomePageDesktop/index.client';
 import { homePageDesktopData } from '../constants/index.server';
 
+import { useShopQuery, gql } from '@shopify/hydrogen';
+
+const queryString = gql`
+  {
+    shop {
+      name
+    }
+  }
+`;
+
 export default function Home() {
+  const { data } = useShopQuery({
+    query: queryString,
+  });
+
+  console.log(data);
+
   return <HomePageDesktop {...homePageDesktopData} />;
 }
